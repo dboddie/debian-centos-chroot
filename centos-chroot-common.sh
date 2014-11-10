232b091b30fe6241e34f304ce3c176362555eac6
David Boddie <david.boddie@met.no>
2014-11-10 16:23:13 +0100

    Made the common settings a bit more generic.

--- centos-chroot-common.sh
+++ centos-chroot-common.sh
@@ -1,5 +1,5 @@
 # Set the CHROOT environment variable to the location of the chroot to be created:
-export CHROOT=/disk1/centos-chroot
+export CHROOT=/tmp/centos-chroot
 # Set the MIRROR environment variable to the location of the release RPM:
 export MIRROR=ftp://ftp.uninett.no/pub/linux/centos/7/os/x86_64/Packages/
 # Set the RELEASE_RPM environment variable to the file name of the release RPM:
